ba88e2ab9e6512e6ac5a78484fd4ef64ce8a465a
fitzk <kayla.fitzsimmons@protonmail.com>
2016-12-11 22:04:00 -0800

    add html template and entry file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Home extends Component {
+  render(){
+    return <div><h1>HACK YOUR MOM</h1></div>
+  }
+}
+
+ReactDOM.render(<Home/>, document.getElementById('app'))
