ee81f42834e9692537c6108ed1d8f4329993f62c
fitzk <kayla.fitzsimmons@protonmail.com>
2016-12-11 22:20:40 -0800

    add bootsrap material page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import {Paper, AppBar, Card} from 'material-ui'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from "react-tap-event-plugin";
+
+injectTapEventPlugin()
 
 export default class Home extends Component {
   render(){
-    return <div><h1>HACK YOUR MOM</h1></div>
+    return (<MuiThemeProvider>
+      <div>
+        <AppBar
+  title="Hack Your Mom"
+  iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+<Paper style={{ display: 'flex', flexDirection: 'column', alignItems:'center', height:'100vh'}}>
+        <Card style={{ marginTop: '10vh', padding: '10px', height:'90vh', width: '70vw'}}>
+        <article>
+          Hacking your mom for her own good jah
+        </article>
+        <article>
+          Hacking your mom for her own good jah
+        </article>
+        <article>
+          Hacking your mom for her own good jah
+        </article>
+      </Card>
+      </Paper>
+    </div>
+     </MuiThemeProvider>)
   }
 }
 
